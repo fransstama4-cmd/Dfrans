0d6775ce645b07397b422f2a5f4a28d2438f7776
fransstama4-cmd <fransstama4@gmail.com>
2025-10-21 13:56:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,7 @@
 tombolSapa.addEventListener('click', function() {
   alert('Halo! Terima kasih sudah berkunjung!');
 });
-// Kode alert sapaan yang sudah ada
-const tombolSapa = document.getElementById('sapaButton');
-tombolSapa.addEventListener('click', function() {
-  alert('Halo! Terima kasih sudah berkunjung!');
-});
 
-
-// KODE UNTUK MODE GELAP-TERANG
 const modeToggle = document.getElementById('mode-toggle'); // 1. Ambil tombolnya
 const body = document.body; // 2. Ambil elemen body
 
