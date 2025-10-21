7d861c613b3cb63273f37a150fd738f59f7b1d05
fransstama4-cmd <fransstama4@gmail.com>
2025-10-21 13:46:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 tombolSapa.addEventListener('click', function() {
   alert('Halo! Terima kasih sudah berkunjung!');
 });
+// Kode alert sapaan yang sudah ada
+const tombolSapa = document.getElementById('sapaButton');
+tombolSapa.addEventListener('click', function() {
+  alert('Halo! Terima kasih sudah berkunjung!');
+});
+
+
+// KODE UNTUK MODE GELAP-TERANG
+const modeToggle = document.getElementById('mode-toggle'); // 1. Ambil tombolnya
+const body = document.body; // 2. Ambil elemen body
+
+// 3. Jalankan fungsi saat tombol di-klik
+modeToggle.addEventListener('click', function() {
+    // 4. Tambahkan/hapus class 'dark-mode' pada body
+    body.classList.toggle('dark-mode');
+});
